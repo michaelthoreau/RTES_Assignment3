a63095891ac5df829447f9e0ecf2110dbc395398
piekarski92 <76534954+piekarski92@users.noreply.github.com>
2021-04-04 23:10:13 -0400

    Update main.cpp
    Revised extension component.

--- main.cpp
+++ main.cpp
@@ -75,9 +75,11 @@ void part3() {
 
 /*	Signal Graphing
 *	Extension - In this exercise you will utilize a timer to monitor the Analog ADC pins and sample their values.
-*	Send your sampled data over your USB serial line to the terminal and graph your result.
-*	For this extension, you will sample the ADC for 5 seconds at 500 samples/second. You may graph your collected data in either
-*	Matlab or Python (Using matplotlib libraries) and submit the resulting graph as part of this assignment.
+*	Set up PB_0 for PwmOut and set up PB_1 for AnalogIn. Initiate a PWM signal of 1Hz with a 20% duty cycle on PB_0.
+*	When sampling, you must short the pins PB_0 and PB_1. One method is to utilize jumper JP2 that comes with your board!
+*	For this extension, you will sample the ADC for 5 seconds at 500 samples/second. Store your samples in a buffer and then output the buffer to the terminal.
+*	From the terminal, record your data to a text file. You may graph your collected data in either Matlab or Python (Using matplotlib libraries)
+*	and submit the resulting graph as a jpg/png as part of this assignment. Remember to label the x & y axis correctly.
 */
 
 void extension() {
